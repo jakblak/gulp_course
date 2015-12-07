3c6b884a79c5d8840d96e7f3edfd8be65e2d6497
Patrick Schroeder <schroe9@gmail.com>
2015-12-06 23:34:47 -0800

    utils.js for logging messages for gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var config = require('./gulp.config')();
+var del = require('del');
+var utilities = require('./utils');
 var $ = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('vet', function() {
@@ -13,10 +15,22 @@ gulp.task('vet', function() {
     .pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', ['clean-styles'], function() {
   return gulp
     .src(config.less)
     .pipe($.less())
     .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
     .pipe(gulp.dest(config.temp));
 });
+
+gulp.task('clean-styles', function() {
+  var files = config.temp + '**/*.css';
+  clean(files);
+});
+
+
+function clean(path) {
+  utilities.logger('Cleaning: ' + $.util.colors.blue(path));
+  del(path);
+}
+
---
+++ utils.js
@@ -0,0 +1,13 @@
+var $ = require('gulp-load-plugins')({lazy: true});
+
+exports.logger = function (msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
