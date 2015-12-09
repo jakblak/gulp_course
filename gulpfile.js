45ab309e2b4befbb96607a159091802c445d003e
Patrick Schroeder <schroe9@gmail.com>
2015-12-08 16:42:25 -0800

    add then comment out browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,9 @@ gulp.task('styles', ['clean-styles'], function(done) {
     .src(config.less)
     .pipe($.less())
     .pipe($.plumber())
-    .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 version', '> 5%']
+    }))
     .pipe(gulp.dest(config.temp));
 });
 
@@ -32,7 +34,7 @@ gulp.task('clean-styles', function() {
 });
 
 gulp.task('wiredep', function() {
-  utilities.log('add bower css, jss and app js files into index.html');
+  utilities.log('Add bower css, jss and app js files into index.html');
   var options = config.getWiredepDefaultOptions();
   var wiredep = require('wiredep').stream;
   var jsFiles = gulp.src(config.js);
@@ -45,7 +47,7 @@ gulp.task('wiredep', function() {
 });
 
 gulp.task('inject', ['wiredep', 'styles'], function() {
-  utilities.log('wire up the app css into the html, and call wiredep ');
+  utilities.log('Wire up the app css into the html, and call wiredep ');
   return gulp
     .src(config.index)
     .pipe($.inject(gulp.src(config.css)))
@@ -64,5 +66,13 @@ gulp.task('serve-dev', ['inject'], function() {
     watch: [config.server]
   };
 
-  return $.nodemon(nodeOptions);
+  return $.nodemon(nodeOptions)
+    .on('restart', function(ev) {
+      utilities.log('*** files changed on restart:\n' + ev);
+    })
+    .on('start', function() {
+      utilities.log('*** nodemon started');
+      // utilities.startBrowserSync();
+    });
+
 });
--- utils.js
+++ utils.js
@@ -1,6 +1,9 @@
+var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({lazy: true});
 var utilities = require('./utils');
+var config = require('./gulp.config')();
 var del = require('del');
+var browserSync = require('browser-sync');
 
 exports.log = function (msg) {
     if (typeof(msg) === 'object') {
@@ -18,3 +21,44 @@ exports.clean = function (path, done) {
   utilities.log('Cleaning: ' + $.util.colors.blue(path));
   del(path, done);
 };
+
+exports.startBrowserSync = function() {
+  if(browserSync.active) {
+    return;
+  }
+  utilities.log('Starting browser-sync on port ' + port);
+
+  gulp.watch([config.less], ['styles'])
+    .on('change', function(event) {
+      utilities.changeEvent(event);
+    });
+
+  var options = {
+    proxy: 'localhost: ' + port,
+    port: 3000,
+    files: [
+      config.client + '**/*.*',
+      '!' + config.less,
+      config.temp + '**/*.css'
+      ],
+    ghostMode: {
+      clicks: true,
+      location: false,
+      forms: true,
+      scroll: true
+    },
+    injectChanges: true ,
+    logFileChanges: true,
+    logLevel: 'debug',
+    logPrefix: 'gulp-patterns',
+    notify: true,
+    reloadDelay: 0 // 1000
+  };
+
+  browserSync(options);
+};
+
+exports.changeEvent = function(event){
+  var srcPattern = new RegExp('/.*(?=/' + config.source + ')/');
+  utilities.log('File ' + event.path.replace(srcPattern, '') + ' ' + event.type);
+}
